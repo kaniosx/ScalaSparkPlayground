86746c0d6c43151d9c7bfa61b3ac8313cac626a8
Mikolaj Kania <mikolaj.kania@freshmail.pl>
2023-01-15 19:00:17 +0100

    FriendsByAge

--- src/main/scala/FriendsByAge.scala
+++ src/main/scala/FriendsByAge.scala
@@ -15,7 +15,7 @@ object FriendsByAge {
     val lines: RDD[String] = sc.textFile("data/fakefriends-noheader.csv")
     val rdd: RDD[(Int, Int)] = lines.map(parseLine)
     val totalsByAge: RDD[(Int, (Int, Int))] = rdd.mapValues(x => (x, 1)).reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
-    val averagesByAge = totalsByAge.mapValues(x => x._1 / x._2)
+    val averagesByAge: RDD[(Int, Int)] = totalsByAge.mapValues(x => x._1 / x._2)
 
     averagesByAge.collect().sortBy(x => x._1).foreach(println)
   }
